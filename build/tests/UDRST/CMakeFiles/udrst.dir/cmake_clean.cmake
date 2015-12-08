7d5b3acdcb44d563df61a80ef4c92bcf9793ce6d
Kenneth <kenneth.a.barr@gmail.com>
2015-12-07 18:00:44 -0600

    Added install.txt and removed installation specific files

--- build/tests/UDRST/CMakeFiles/udrst.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/udrst"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/udrst.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
